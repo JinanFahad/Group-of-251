d56b648e123feb1be4d19e9ae9ea48eebf097015
Renas Khalid <renas6551@gmail.com>
2024-10-30 09:48:59 +0300

    new commit

---
+++ src/main/java/com/mycompany/project251/group4_a2/Project251Group4_A2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.project251.group4_a2;
+
+/**
+ *
+ * @author renas
+ */
+public class Project251Group4_A2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
