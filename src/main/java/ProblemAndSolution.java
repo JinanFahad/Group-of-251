e86bc4210d8d0f45d46e9a08bc423e4cbc714caa
zenaaj <jaredzena@gmail.com>
2024-11-06 20:58:29 +0300

    initialized methods and attributes for ProblemAndSolution class

---
+++ src/main/java/ProblemAndSolution.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class ProblemAndSolution {
+
+    private String problemDescription;
+    private List<String> keywords;
+    private String solution;
+
+    public ProblemAndSolution(String problemDescription, List<String> keywords, String solution) {
+        this.problemDescription = problemDescription;
+        this.keywords = keywords;
+        this.solution = solution;
+    }
+
+    public String getProblemDescription() {
+        return problemDescription;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+}
