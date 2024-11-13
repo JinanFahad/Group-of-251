72e961506b9a47a3cb86ca8e063e80192f37cffc
zenaaj <jaredzena@gmail.com>
2024-11-13 12:31:21 +0400

    Merge origin/master
    Conflicts:
    	src/main/java/ProblemAndSolution.java

--- src/main/java/KeywordBasedSearchSystem.java
+++ src/main/java/KeywordBasedSearchSystem.java
@@ -1,4 +1,32 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class KeywordBasedSearchSystem {
+    private ProblemSolutionFile problemSolutionFile;
+
+    public KeywordBasedSearchSystem(ProblemSolutionFile problemSolutionFile) {
+        this.problemSolutionFile = problemSolutionFile;
+    }
     
+    public String searchProblem(String description) throws IOException {
+        List<String> keywords = extractKeywords(description);
+        for (String keyword : keywords) {
+            List<ProblemAndSolution> matchingProblems = problemSolutionFile.searchInFile(keyword);
+            if (!matchingProblems.isEmpty()) {
+                return matchingProblems.get(0).getSolution();
+            }
+        }
+        return "No matching solution found. Please contact the support team.";
+    }
+    
+    private List<String> extractKeywords(String description) {
+       
+        String[] keywords;
+        keywords = description.toLowerCase().split("\\s+");
+        return Arrays.asList(keywords); 
+    }
+
+
+   
 }
