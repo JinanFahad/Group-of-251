a2fdfeeb1299713f66e22503e822f8b0fd3f63e4
Jinan Fahad <jbalmidfa@stu.kau.edu.sa>
2024-11-16 01:44:40 +0300

    Merge origin/master
    # Conflicts:
    #	src/main/java/ProblemSolutionFile.java

--- src/main/java/SupportTechnician.java
+++ src/main/java/SupportTechnician.java
@@ -1,4 +1,21 @@
+import java.io.IOException;
+import java.util.List;
 
 public class SupportTechnician {
-    
+     private List<String> teamMembers;
+    private ProblemSolutionFile problemSolutionFile;
+
+    public SupportTechnician(List<String> teamMembers, ProblemSolutionFile problemSolutionFile) {
+        this.teamMembers = teamMembers;
+        this.problemSolutionFile = problemSolutionFile;
+    }
+ public void addProblem(ProblemAndSolution problem) {
+        try {
+            problemSolutionFile.writeProblem(problem);
+            System.out.println("Problem added successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while adding the problem.");
+        }
+    }
+
 }
