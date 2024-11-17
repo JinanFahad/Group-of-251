07bdc6002688c9e5c1e34891e6b1b63a3200862d
Jinan Fahad <jbalmidfa@stu.kau.edu.sa>
2024-11-17 23:49:35 +0300

    Merge origin/master
    # Conflicts:
    #	src/main/java/ProblemSolutionFile.java
    
    Add UpdateProblem method

--- src/main/java/SupportTechnician.java
+++ src/main/java/SupportTechnician.java
@@ -1,21 +1,38 @@
+
 import java.io.IOException;
 import java.util.List;
 
 public class SupportTechnician {
-     private List<String> teamMembers;
+
+    private List<String> teamMembers;
     private ProblemSolutionFile problemSolutionFile;
 
     public SupportTechnician(List<String> teamMembers, ProblemSolutionFile problemSolutionFile) {
         this.teamMembers = teamMembers;
         this.problemSolutionFile = problemSolutionFile;
     }
- public void addProblem(ProblemAndSolution problem) {
+
+    public void addProblem(ProblemAndSolution problem) {
         try {
             problemSolutionFile.writeProblem(problem);
             System.out.println("Problem added successfully.");
         } catch (IOException e) {
             System.out.println("An error occurred while adding the problem.");
         }
+    }
+
+    public void updateProblem(ProblemAndSolution problem) {
+        try {
+            problemSolutionFile.updateProblem(problem);
+            System.out.println("Problem updated successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while updating the problem.");
+        }
+    }
+
+    public void deleteProblem(String problemId) {
+
+        System.out.println("Problem deletion functionality is not yet implemented.");
     }
 
 }
