edbb8862e9e6bc56bccef43d6f9927299dcae405
Jouri <jourialmutairi11@gmail.com>
2024-12-04 16:12:20 +0300

    requestSolution method has been added

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -12,9 +12,11 @@ public class Employee {
     
 
     public void requestSolution(String problemDescription) {
+        
         try {
             String solution = searchSystem.searchProblem(problemDescription);
             System.out.println("Solution: " + solution);
+            
         } catch (IOException e) {
             System.out.println("An error occurred while searching for a solution.");
         }
